32fb944c4997176f2cd3f54518d62ed6165af019
Yannik195 <yannisim@gmail.com>
2024-03-27 00:58:00 +0100

    counter

--- src/Item.jsx
+++ src/Item.jsx
@@ -4,12 +4,27 @@ import styles from "./Item.module.css";
 
 function Item({ getränk }) {
   const [count, setCount] = useState(1);
+
+  const handleIncreaseCount = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
     <div className={styles.item}>
-      <div className={styles.count}>{count > 0 && count}</div>
+      <div className={styles.count} onClick={handleDecreaseCount}>
+        {count > 0 && count}
+      </div>
       <span className={styles.volume}>{getränk.volume}l</span>
       <span className={styles.name}>{getränk.name}</span>
-      <div className={styles.price}>€{getränk.price} </div>
+      <div className={styles.price} onClick={handleIncreaseCount}>
+        €{getränk.price}{" "}
+      </div>
     </div>
   );
 }
