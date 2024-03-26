8396d8bdb61771e5069497d1ba0108979d9782e3
Yannik195 <yannisim@gmail.com>
2024-03-27 00:41:03 +0100

    flex

--- src/Item.jsx
+++ src/Item.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 import styles from "./Item.module.css";
 
 function Item({ getränk }) {
+  const [count, setCount] = useState(1);
   return (
     <div className={styles.item}>
+      <div className={styles.count}>{count > 0 && count}</div>
       <span className={styles.volume}>{getränk.volume}l</span>
       <span className={styles.name}>{getränk.name}</span>
-      <span className={styles.price}>€{getränk.price} </span>
+      <div className={styles.price}>€{getränk.price} </div>
     </div>
   );
 }
